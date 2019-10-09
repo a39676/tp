c445fbd8b41b7f0094cc2177c719af8a0dd03534
q39676 <396767101@qq.com>
2019-10-09 17:43:37 +0800

    .

--- src/main/java/appium/Appium.java
+++ src/main/java/appium/Appium.java
@@ -3,7 +3,6 @@ package appium;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
-import java.util.Map;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -12,26 +11,6 @@ import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 
 public class Appium {
-	
-//	public static void main(String[] args) throws MalformedURLException, InterruptedException {
-//
-//        DesiredCapabilities capabilities = new DesiredCapabilities();
-//        capabilities.setCapability("deviceName", "Galaxy Nexus API 29_06");
-//        capabilities.setCapability("automationName", "Appium");
-//        capabilities.setCapability("platformName", "Android");
-//        capabilities.setCapability("platformVersion", "10");
-//        capabilities.setCapability("appPackage", "com.komorebi.memo");
-//        capabilities.setCapability("appActivity", "com.komorebi.memo.MainActivity");
-//
-//        @SuppressWarnings("rawtypes")
-//		AndroidDriver androidDriver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
-//
-//        Map<String, String> map = androidDriver.getAppStringMap();
-//        
-//        System.out.println(map);
-//
-//        androidDriver.quit();
-//    }
 	
 	@SuppressWarnings("rawtypes")
 	public static void main(String[] args) throws MalformedURLException, InterruptedException {
@@ -50,9 +29,6 @@ public class Appium {
         try {
         	d = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
         	
-        	Map<String, String> map = d.getAppStringMap();
-        	System.out.println(map);
-
         	List cjvList = d.findElements(By.id("cjv"));
         	AndroidElement tmp = null;
         	AndroidElement workTable = null;
