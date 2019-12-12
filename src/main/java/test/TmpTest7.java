9c76bc42f407d6ee248cde1ffe1a47012dd8a696
q39676 <396767101@qq.com>
2019-12-12 17:34:15 +0800

    .

--- src/main/java/test/TmpTest7.java
+++ src/main/java/test/TmpTest7.java
@@ -1,26 +1,17 @@
 package test;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 public class TmpTest7 {
 	
 	public static void main(String[] args) {
-		List<String> l = List.of("1", "2", "3", "4", "5", "6", "7", "8", "9", "10");
+		LocalDateTime n = LocalDateTime.now();
+		LocalDateTime n2 = LocalDateTime.now().plusDays(2);
 		
-		List<List<String>> mainList = new ArrayList<List<String>>();
-		int step = 3;
-		List<String> tmpList = null;
-		for(int i = 0; i < l.size(); i = i + step) {
-			tmpList = new ArrayList<String>();
-			if(i + step > l.size()) {
-				tmpList = l.subList(i, l.size());
-			} else {
-				tmpList = l.subList(i, i + step);
-			}
-			mainList.add(tmpList);
-			System.out.println(tmpList);
-		}
+		long d = ChronoUnit.DAYS.between(n2, n);
+		
+		System.out.println(d);
 		
 	}
 
