867993163d0159610d217f0cb130d9780809fc51
a39676 <396767101@qq.com>
2019-12-18 16:39:35 +0800

    .

--- src/main/java/test/TmpTest7.java
+++ src/main/java/test/TmpTest7.java
@@ -6,13 +6,8 @@ import java.time.temporal.ChronoUnit;
 public class TmpTest7 {
 	
 	public static void main(String[] args) {
-		LocalDateTime n = LocalDateTime.now();
-		LocalDateTime n2 = LocalDateTime.now().plusDays(2);
-		
-		long d = ChronoUnit.DAYS.between(n2, n);
-		
-		System.out.println(d);
-		
+		String s = "a" + System.lineSeparator() + "b";
+		System.out.println(s);
 	}
 
 }
