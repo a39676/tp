35572ece3d97b01f881a0d413040519733892b66
a39676 <396767101@qq.com>
2019-12-19 17:45:13 +0800

    .

--- src/main/java/job_test/dfsw/BuildSVNUpdateCommondLine.java
+++ src/main/java/job_test/dfsw/BuildSVNUpdateCommondLine.java
@@ -2,9 +2,13 @@ package job_test.dfsw;
 
 public class BuildSVNUpdateCommondLine {
 	
-	public String replace(String lineInput) {
+	// 本地存放代码的路径前缀
+	private static String pathPrefix = "D:/work/合同系统/03实现/001程序代码";
+	
+	private static String replace(String lineInput) {
+		// svn 更新命令的前缀
 		String svnPrefix = "svn update ";
-		String pathPrefix = "D:/work/合同系统/03实现/001程序代码";
+		// 定位匹配字符替换的关键字
 		String targetStr = "001程序代码";
 		
 		int index = lineInput.indexOf(targetStr);
@@ -17,7 +21,7 @@ public class BuildSVNUpdateCommondLine {
 		return svnPrefix + pathPrefix + suffix;
 	}
 	
-	public void batchReplace(String lines) {
+	private static void batchReplace(String lines) {
 		String[] lineArray = lines.split(System.lineSeparator());
 		for(String line : lineArray) {
 			System.out.println(replace(line));
@@ -27,11 +31,9 @@ public class BuildSVNUpdateCommondLine {
 
 	public static void main(String[] args) {
 		
-		BuildSVNUpdateCommondLine l = new BuildSVNUpdateCommondLine();
-		
 		String lines = "Modified : /1开发库/企业组/集团合同管理系统(二期)/工程类/03实现/001程序代码/平台代码/OT.Platform/OT.OPF.Web/Context/UserContext.cs\r\n" + 
 				"Modified : /1开发库/企业组/集团合同管理系统(二期)/工程类/03实现/001程序代码/平台代码/OT.Platform/OT.OPF.Web/Controllers/HomeController.cs";
 		
-		l.batchReplace(lines);
+		batchReplace(lines);
 	}
 }
