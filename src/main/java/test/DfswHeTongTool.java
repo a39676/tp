3588497aa5c03941cc4e9cfbfce7f1cd8a2955b5
a39676 <396767101@qq.com>
2020-01-20 16:59:51 +0800

    .

--- src/main/java/test/DfswHeTongTool.java
+++ src/main/java/test/DfswHeTongTool.java
@@ -10,16 +10,13 @@ import toolPack.ioHandle.FileUtilCustom;
 
 public class DfswHeTongTool {
 	
-	private static final String sourceBinPath = "D:/Cnt/bin";
-	
+	private static final String outputPackPath = "d:/Cnt";
+	private static final String outputBinPath = outputPackPath + "/bin";
 	private static final String updateFilePath = "d:/tmp/updates.txt";
-	
 	private static final String localPathPrefix = "D:/work/合同系统/03实现/001程序代码";
-	
 	private static final String copyToTargetFolderPath = "d:/tmp/copyTarget2";
 	
 	public static void main(String[] args) {
-		List<String> targetFilePath = new ArrayList<String>();
 		
 		List<String> sourceLines = findSourceFile();
 		List<String> lines = new ArrayList<String>();
@@ -30,60 +27,54 @@ public class DfswHeTongTool {
 		}
 		
 		Collections.sort(lines);
+		
+		List<String> localSourceCodeUpdateFilePathList = new ArrayList<String>();
+		for(String s : lines) {
+			localSourceCodeUpdateFilePathList.add(buildLocalFilePathFromBugFreeSource(localPathPrefix, s));
+		}
+		for(String s : localSourceCodeUpdateFilePathList) {
+			System.out.println(s);
+		}
 	
-		System.out.println(lines.size());
-		List<String> svnCommondLines = buildSVNUpdateCommondLine(lines);
-		for(String l : svnCommondLines) {
-			System.out.println(l);
+		List<String> svnCommondLines = buildSVNUpdateCommondLine(localSourceCodeUpdateFilePathList);
+		for(String s : svnCommondLines) {
+			System.out.println(s);
 		}
+		List<String> allDLLFileNames = findAllDllNames();
 		
-		List<String> localSourceCodeFilePathList = getLocalPathListFromSvnCommondLines(svnCommondLines);
-//		for(String l : filePathList) {
-//			System.out.println(l);
-//		}
+		List<String> dllFilePathList = filterTargetDLLFilePath(localSourceCodeUpdateFilePathList, allDLLFileNames);
+		List<String> pdbFilePathList = filterTargetPDBFilePath(localSourceCodeUpdateFilePathList, allDLLFileNames, dllFilePathList);
+		List<String> frontSideOutputFilePathList = filterFrontSideFilePath(localSourceCodeUpdateFilePathList);
 		
-		List<String> dllFileNames = findAllDllNames();
-		System.out.println(dllFileNames);
-		
-		String dllPath = null;
-		List<String> dllFilePathList = new ArrayList<String>();
-		List<String> pdbFilePathList = new ArrayList<String>();
-		List<String> frontSideFilePathList = new ArrayList<String>();
-		
-		String tmpPdbFilePath = null;
-		for(String l : localSourceCodeFilePathList) {
-			if(l.endsWith(".cs")) {
-				dllPath = findTargetDll(l, dllFileNames);
-				if(!dllFilePathList.contains(dllPath)) {
-					dllFilePathList.add(dllPath);
-					if(!l.contains("平台代码")) {
-						tmpPdbFilePath = findTargetPdbFile(dllPath);
-						if(tmpPdbFilePath != null && !pdbFilePathList.contains(tmpPdbFilePath)) {
-							pdbFilePathList.add(tmpPdbFilePath);
-						}
-					}
-				}
-			} else {
-				if(!frontSideFilePathList.contains(l)) {
-					frontSideFilePathList.add(l.replaceAll("\\\\", "/"));
-				}
-			}
-		}
-		
-		targetFilePath.addAll(dllFilePathList);
-		targetFilePath.addAll(pdbFilePathList);
-		targetFilePath.addAll(frontSideFilePathList);
-		
-		Collections.sort(targetFilePath);
 		List<String> copyTargetList = new ArrayList<String>();
-		for(String l : targetFilePath) {
-			if(l.endsWith("dll") || l.endsWith("pdb")) {
-				System.out.println(l.replaceAll(sourceBinPath, "/OT.WebSite/bin"));
-				copyTargetList.add(copyToTargetFolderPath + l.replaceAll(sourceBinPath, "/OT.WebSite/bin"));
-			} else if(l.startsWith(localPathPrefix)) {
-				System.out.println(l.replaceAll(localPathPrefix + "/OT.CNT", ""));
-				copyTargetList.add(copyToTargetFolderPath + l.replaceAll(localPathPrefix + "/OT.CNT", ""));
-			}
+		
+		List<String> backGroundFilePath = new ArrayList<String>();
+		backGroundFilePath.addAll(dllFilePathList);
+		backGroundFilePath.addAll(pdbFilePathList);
+		
+		List<String> frontGroundFilePath = new ArrayList<String>();
+		frontGroundFilePath.addAll(frontSideOutputFilePathList);
+		
+		Collections.sort(backGroundFilePath);
+		Collections.sort(frontGroundFilePath);
+		
+		List<String> copyCommondLine = new ArrayList<String>();
+		
+		String subPath = null;
+		List<String> subPathList = new ArrayList<String>();
+		
+		for(String l : backGroundFilePath) {
+			subPath = l.replaceAll(outputBinPath, "/OT.WebSite/bin");
+			copyTargetList.add(copyToTargetFolderPath + subPath);
+			copyCommondLine.add("cp " + l + " " + copyToTargetFolderPath + subPath);
+			subPathList.add(subPath);
+		}
+		
+		for(String l : frontGroundFilePath) {
+			subPath = l.replaceAll(outputPackPath, "");
+			copyTargetList.add(copyToTargetFolderPath + subPath);
+			copyCommondLine.add("cp " + l + " " + copyToTargetFolderPath + subPath);
+			subPathList.add("/OT.WebSite" + subPath);
 		}
 		
 		File f = null;
@@ -92,20 +83,33 @@ public class DfswHeTongTool {
 			if(!f.getParentFile().exists()) {
 				f.getParentFile().mkdirs();
 			}
-			System.out.println(l);
+		}
+		
+		for(String s : copyCommondLine) {
+			System.out.println(s);
+		}
+		
+		String numberStr = null;
+		for(int i = 0; i < subPathList.size(); i++) {
+			if(i < 10) {
+				numberStr = "0" + i;
+			} else {
+				numberStr = String.valueOf(i);
+			}
+			System.out.println(numberStr + ": " + subPathList.get(i));
 		}
 	}
 
-	public static List<String> findSourceFile() {
+	private static List<String> findSourceFile() {
 		FileUtilCustom io = new FileUtilCustom();
 		String sourceStr = io.getStringFromFile(updateFilePath);
 		List<String> lines = Arrays.asList(sourceStr.split(System.lineSeparator()));
 		return lines;
 	}
 	
-	public static List<String> findAllDllNames() {
+	private static List<String> findAllDllNames() {
 		
-		File projectBinFolder = new File(sourceBinPath);
+		File projectBinFolder = new File(outputBinPath);
 		
 		List<File> sourceFiles = Arrays.asList(projectBinFolder.listFiles());
 		List<String> dllFileNameList = new ArrayList<String>();
@@ -118,7 +122,7 @@ public class DfswHeTongTool {
 		return dllFileNameList;
 	}
 	
-	public static String findTargetPdbFile(String dllFilePath) {
+	private static String findTargetPdbFile(String dllFilePath) {
 		
 		File pdbFile = new File(dllFilePath.replaceAll(".dll", ".pdb"));
 		if(pdbFile.exists()) {
@@ -127,17 +131,17 @@ public class DfswHeTongTool {
 		return null;
 	}
 	
-	public static String findTargetDll(String filePath, List<String> dllNames) {
+	private static String findTargetDll(String filePath, List<String> dllNames) {
 		
 		File f = new File(filePath);
 		if("OT.CNT.Web".equals(f.getName())) {
-			return sourceBinPath + "/OT.CNT.WebController.dll";
+			return outputBinPath + "/OT.CNT.WebController.dll";
 		}
 		if(f.getName().equals("OT.Workflow3.APIs")) {
 			System.out.println("");
 		}
 		if(dllNames.contains(f.getName())) {
-			return sourceBinPath + "/" + f.getName() + ".dll";
+			return outputBinPath + "/" + f.getName() + ".dll";
 		} else if(f.getParentFile() != null) {
 			return findTargetDll(f.getParentFile().getAbsolutePath(), dllNames);
 		}
@@ -145,22 +149,15 @@ public class DfswHeTongTool {
 		return null;
 	}
 	
-	public static List<String> buildSVNUpdateCommondLine(List<String> inputLines) {
-		List<String> resultLines = batchReplaceToSVNUpdateCommondLine(inputLines);
-		return resultLines;
-	}
-	
-	private static List<String> batchReplaceToSVNUpdateCommondLine(List<String> lines) {
+	private static List<String> buildSVNUpdateCommondLine(List<String> inputLines) {
 		List<String> resultLines = new ArrayList<String>();
-		String resultLine = null;
-		for (String sourceLine : lines) {
-			resultLine = replaceToSVNUpdateCommondLine(localPathPrefix, sourceLine);
-			resultLines.add(resultLine);
+		for(String s : inputLines) {
+			resultLines.add("svn update " + s);
 		}
 		return resultLines;
 	}
 	
-	private static String replaceToSVNUpdateCommondLine(String localPath, String bugFreePath) {
+	private static String buildLocalFilePathFromBugFreeSource(String localPath, String bugFreePath) {
 		List<String> localPathSplit = Arrays.asList(localPath.split("/"));
 		List<String> bugFreeSplit = Arrays.asList(bugFreePath.split("/"));
 		
@@ -179,7 +176,7 @@ public class DfswHeTongTool {
 			return "";
 		}
 		
-		StringBuffer sb = new StringBuffer("svn update ");
+		StringBuffer sb = new StringBuffer();
 		for(int i = 0; i < localPathIndex; i++) {
 			sb.append(localPathSplit.get(i) + File.separator);
 		}
@@ -193,11 +190,54 @@ public class DfswHeTongTool {
 		return result;
 	}
 
-	public static List<String> getLocalPathListFromSvnCommondLines(List<String> svnCommondLines) {
-		List<String> result = new ArrayList<String>();
-		for(String s : svnCommondLines) {
-			result.add(s.replaceAll("svn update ", ""));
+	private static List<String> filterTargetDLLFilePath(List<String> localSourceCodeUpdateFilePathList, List<String> allDLLFileNames) {
+		String dllPath = null;
+		List<String> dllFilePathList = new ArrayList<String>();
+		for(String l : localSourceCodeUpdateFilePathList) {
+			if(l.endsWith(".cs")) {
+				dllPath = findTargetDll(l, allDLLFileNames);
+				if(!dllFilePathList.contains(dllPath)) {
+					dllFilePathList.add(dllPath);
+				}
+			}
 		}
-		return result;
+		
+		return dllFilePathList;
+	}
+	
+	private static List<String> filterTargetPDBFilePath(List<String> localSourceCodeUpdateFilePathList, List<String> allDLLFileNames, List<String> dllFilePathList) {
+		String dllPath = null;
+		List<String> pdbFilePathList = new ArrayList<String>();
+		
+		String tmpPdbFilePath = null;
+		for(String l : localSourceCodeUpdateFilePathList) {
+			if(l.endsWith(".cs")) {
+				dllPath = findTargetDll(l, allDLLFileNames);
+				if(dllFilePathList.contains(dllPath)) {
+					if(!l.contains("平台代码")) {
+						tmpPdbFilePath = findTargetPdbFile(dllPath);
+						if(tmpPdbFilePath != null && !pdbFilePathList.contains(tmpPdbFilePath)) {
+							pdbFilePathList.add(tmpPdbFilePath);
+						}
+					}
+				}
+			}
+		}
+		
+		return pdbFilePathList;
+	}
+
+	private static List<String> filterFrontSideFilePath(List<String> localSourceCodeUpdateFilePathList) {
+		List<String> frontSideFilePathList = new ArrayList<String>();
+		for(String l : localSourceCodeUpdateFilePathList) {
+			if(!l.endsWith(".cs")) {
+				if(!frontSideFilePathList.contains(l)) {
+					l = l.replaceAll("\\\\", "/").replaceAll(localPathPrefix + "/OT.CNT/OT.WebSite", outputPackPath);
+					frontSideFilePathList.add(l);
+				}
+			}
+		}
+		
+		return frontSideFilePathList;
 	}
 }
--- src/main/java/test/TmpTest8.java
+++ src/main/java/test/TmpTest8.java
@@ -1,55 +1,19 @@
 package test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.math.BigDecimal;
 
 public class TmpTest8 {
 	
 	public static void main(String[] args) throws Exception {
 		
-		List<String> l = new ArrayList<String>();
-		String str = "/OT.WebSite/bin/OT.CNT.WebController.dll\r\n" + 
-				"/OT.WebSite/bin/OT.CNT.WebController.pdb\r\n" + 
-				"/OT.WebSite/bin/OT.CNT.ApplicationServices.dll\r\n" + 
-				"/OT.WebSite/bin/OT.CNT.ApplicationServices.pdb\r\n" + 
-				"/OT.WebSite/bin/OT.CNT.Repository.dll\r\n" + 
-				"/OT.WebSite/bin/OT.CNT.Repository.pdb\r\n" + 
-				"/OT.WebSite/bin/OT.CNT.IAPIs.dll\r\n" + 
-				"/OT.WebSite/bin/OT.CNT.IAPIs.pdb\r\n" + 
-				"/OT.WebSite/bin/OT.Workflow.IAPIs.dll\r\n" + 
-				"/OT.WebSite/bin/OT.Workflow3.APIs.dll\r\n" + 
-				"/OT.WebSite/bin/OT.OPF.dll\r\n" + 
-				"/OT.WebSite/bin/OT.OPF.IAPIs.dll\r\n" + 
-				"/OT.WebSite/bin/OT.CNT.WebController.dll\r\n" + 
-				"/OT.WebSite/Areas/CNT/Views/Contract/ContractArchived/BatchEdit.cshtml\r\n" + 
-				"/OT.WebSite/Areas/CNT/Views/Contract/ContractArchived/List.cshtml\r\n" + 
-				"/OT.WebSite/Areas/CNT/Views/Contract/Cancel/Details.cshtml\r\n" + 
-				"/OT.WebSite/Areas/CNT/Views/Contract/ReceivePayInfo/PaymentApplyEdit.cshtml\r\n" + 
-				"/OT.WebSite/Areas/CNT/Views/Contract/End/Details.cshtml\r\n" + 
-				"/OT.WebSite/Areas/CNT/Views/Contract/End/Edit.cshtml\r\n" + 
-				"/OT.WebSite/Areas/CNT/Views/Contract/End/EndView.cshtml\r\n" + 
-				"/OT.WebSite/Areas/CNT/Views/Shared/_ContractPartInfo.cshtml\r\n" + 
-				"/OT.WebSite/Areas/CNT/Views/Shared/_Cancling.cshtml\r\n" + 
-				"/OT.WebSite/Areas/CNT/Views/Shared/_PaymentApplyDetail.cshtml\r\n" + 
-				"/OT.WebSite/Areas/CNT/Views/Home/WarningList.cshtml\r\n" + 
-				"/OT.WebSite/Areas/CNT/Reports/ContractEnd.rdlc\r\n" + 
-				"/OT.WebSite/Areas/CNT/Reports/ContractChange.rdlc\r\n" + 
-				"/OT.WebSite/Areas/CNT/Reports/ContractPayment.rdlc\r\n" + 
-				"/OT.WebSite/Areas/CNT/Reports/ContractBalance.rdlc\r\n" + 
-				"/OT.WebSite/Areas/CNT/Reports/ContractSign.rdlc\r\n" + 
-				"/OT.WebSite/Areas/CNT/Reports/ContractPayment_IsNotCnt.rdlc\r\n" + 
-				"/OT.WebSite/Areas/CNT/Views/Base/SignedObject/Edit.cshtml\r\n" + 
-				"/OT.WebSite/Areas/CNT/Views/Base/SignedObject/SignedObjectExInfoEdit.cshtml\r\n" + 
-				"/OT.WebSite/Areas/CNT/Views/Base/SignedObjectCreditLimit/SignedObjectSelect.cshtml\r\n" + 
-				"/OT.WebSite/Views/Shared/SubFlowApprove.cshtml\r\n" + 
-				"/OT.WebSite/Images/yizuofei.png"
-				;
-		l.addAll(Arrays.asList(str.split(System.lineSeparator())));
-		Collections.sort(l);
-		for(String s : l) {
-			System.out.println(s);
+		BigDecimal c = new BigDecimal("0.01");
+		BigDecimal total = BigDecimal.ZERO;
+		
+		for(int i = 1; i < 31; i++) {
+			c = c.multiply(new BigDecimal(2));
+			total = total.add(c);
+			
+			System.out.println(i + " : " + c + " : " + total);
 		}
 	}
 
