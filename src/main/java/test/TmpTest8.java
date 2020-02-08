93deb7e9bff6f5883a65e46b631381d849724dac
DavenChan <396767101@qq.com>
2020-02-08 19:45:09 +0800

    .

--- src/main/java/test/TmpTest8.java
+++ src/main/java/test/TmpTest8.java
@@ -1,14 +1,23 @@
 package test;
 
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.util.Base64;
+
 public class TmpTest8 {
-	
+
 	public static void main(String[] args) throws Exception {
-		
-		String desc = "12500000（含加奖2500000）";
-		String[] s = desc.split("[^0-9]");
-		for(String i : s) {
-			System.out.println(i);
-		}
+		System.out.println("s");
+		KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
+		kpg.initialize(4096);
+		KeyPair kp = kpg.generateKeyPair();
+
+		System.out.println("-----BEGIN PRIVATE KEY-----");
+		System.out.println(Base64.getMimeEncoder().encodeToString(kp.getPrivate().getEncoded()));
+		System.out.println("-----END PRIVATE KEY-----");
+		System.out.println("-----BEGIN PUBLIC KEY-----");
+		System.out.println(Base64.getMimeEncoder().encodeToString(kp.getPublic().getEncoded()));
+		System.out.println("-----END PUBLIC KEY-----");
 	}
 
 }
