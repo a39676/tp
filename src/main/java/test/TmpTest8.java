3c49d8fd5e5191b60361cbb71257d0c40dbc3b76
DavenChan <396767101@qq.com>
2020-03-03 23:55:00 +0800

    .

--- src/main/java/test/TmpTest8.java
+++ src/main/java/test/TmpTest8.java
@@ -1,23 +1,15 @@
 package test;
 
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.util.Base64;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 public class TmpTest8 {
 
-	public static void main(String[] args) throws Exception {
-		System.out.println("s");
-		KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
-		kpg.initialize(4096);
-		KeyPair kp = kpg.generateKeyPair();
-
-		System.out.println("-----BEGIN PRIVATE KEY-----");
-		System.out.println(Base64.getMimeEncoder().encodeToString(kp.getPrivate().getEncoded()));
-		System.out.println("-----END PRIVATE KEY-----");
-		System.out.println("-----BEGIN PUBLIC KEY-----");
-		System.out.println(Base64.getMimeEncoder().encodeToString(kp.getPublic().getEncoded()));
-		System.out.println("-----END PUBLIC KEY-----");
+	public static void main(String[] args) throws UnsupportedEncodingException {
+		String pk = "sMDE45RjV0Tqse2akvoF8zOAQeqgi+ZGUJitnOpgTwQ=";
+		String urlEncodePK = URLEncoder.encode(pk, StandardCharsets.UTF_8.toString());
+		System.out.println(urlEncodePK);
 	}
 
 }
