eb18667f2ffdfc3999521df9d729f3bcdabd641c
DavenChan <396767101@qq.com>
2020-12-24 23:27:12 +0800

    .

---
+++ src/main/java/test/TmpTest11.java
@@ -0,0 +1,10 @@
+package test;
+
+import java.util.concurrent.TimeUnit;
+
+public class TmpTest11 {
+
+	public static void main(String[] args) {
+		System.out.println(TimeUnit.DAYS);
+	}
+}
--- src/main/java/test/TmpTest9.java
+++ src/main/java/test/TmpTest9.java
@@ -2,4 +2,7 @@ package test;
 
 public class TmpTest9 {
 
+	public static void main(String[] args) {
+	}
+
 }
