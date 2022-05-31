cde13fa493713b84aba132600b60dabef57edecc
DavenChan <q39676@gmail.com>
2022-05-31 19:18:36 +0800

    .

---
+++ src/main/java/test/LongNumberDemo.java
@@ -0,0 +1,12 @@
+package test;
+
+public class LongNumberDemo {
+
+	public static void main(String[] args) throws Exception {
+
+		Long longNumber = 1234_5678_2234L;
+		System.out.println(longNumber);
+		Integer longInt = 1234_5678;
+		System.out.println(longInt);
+	}
+}
