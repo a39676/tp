ef0c10045ee0716da20330f05dc2fcb7df17e889
DavenChan <q39676@gmail.com>
2022-08-14 11:17:58 +0800

    .

--- src/main/java/test/TmpTest16.java
+++ src/main/java/test/TmpTest16.java
@@ -1,8 +1,20 @@
 package test;
 
+import java.util.Base64;
+
 public class TmpTest16 {
 
 	public static void main(String[] args) throws Exception {
-		
+
+		String encodedString = "Z3VvcnVpMjMzMzMz";
+		byte[] decodedBytes = Base64.getDecoder().decode(encodedString);
+		String decodedString = new String(decodedBytes);
+		System.out.println(decodedString);
+
+		encodedString = "bHJqMDA3QGdtYWlsLmNvbQ==";
+		decodedBytes = Base64.getDecoder().decode(encodedString);
+		decodedString = new String(decodedBytes);
+		System.out.println(decodedString);
+
 	}
 }
