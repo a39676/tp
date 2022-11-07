cd960fddf04f9f7e0c3ae050eeefda703be7f738
DavenChan <demo@demo.com>
2022-11-07 14:20:00 +0800

    .

--- src/main/java/test/TmpTest17.java
+++ src/main/java/test/TmpTest17.java
@@ -1,11 +1,8 @@
 package test;
 
-import java.util.Date;
-
 public class TmpTest17 {
 
 	public static void main(String[] args) {
-		Date d = new Date(1665310043);
-		System.out.println(d);
+
 	}
 }
