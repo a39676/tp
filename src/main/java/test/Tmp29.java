20e307aa0ae726e4b58296ea5b4a968523119c10
DavenChan <19781485+a39676@users.noreplay.github.com>
2023-09-25 09:32:46 +0800

    .

---
+++ src/main/java/test/Tmp29.java
@@ -0,0 +1,11 @@
+package test;
+
+import java.time.LocalDateTime;
+
+public class Tmp29 {
+
+	public static void main(String[] args) {
+		LocalDateTime start = LocalDateTime.of(2023, 6, 30, 0, 0);
+		System.out.println(start.plusDays(100));
+	}
+}
