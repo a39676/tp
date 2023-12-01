6f6e62a83baf4c0118330e996b6211ed65d51be8
Daven <19781485+a39676@users.noreply.github.com>
2023-12-01 11:19:26 +0800

    Update

--- src/main/java/test/Typing.java
+++ src/main/java/test/Typing.java
@@ -10,6 +10,9 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Typing {
 
@@ -23,15 +26,34 @@ public class Typing {
 		} catch (AWTException e) {
 			e.printStackTrace();
 		}
+
+		System.out.println("Hi");
+
 		String str = null;
-//		str= "<plugin><groupId>org.apache.maven.plugins.boot</groupId><artifactId>maven-resources-plugin</artifactId><version>3.1.0</version>/plugin>";
-		
-//		str = t.getStringFromFile("D:\\myMavenProject\\ToolPacks\\src\\main\\java\\toolPack\\ioHandle/FileUtilCustom.java");
+//		str = "abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ `1234567890-=[];',./ ~!@#$%^&*()_+{}:\"<>?"
+//				+ "	";
+
+//		str= "Basic Field Validation for Excel Upload";
+
 		str = t.getStringFromFile("D:/tmp/tmp.txt");
-		
-		t.type(str);
+
+		List<String> strList = new ArrayList<>();
+		strList.addAll(Arrays.asList(str.split("")));
+		String tmpS = null;
+		for (int i = 0; i < strList.size(); i++) {
+			tmpS = strList.get(i);
+			if ("\r".equals(tmpS) && (i < strList.size() - 1) && ("\n".equals(strList.get(i + 1)))) {
+				t.type(String.valueOf("\r\n"));
+				i++;
+			} else {
+				t.type(String.valueOf(tmpS));
+			}
+			Thread.sleep(20L);
+		}
+
+		System.out.println("End");
 	}
-	
+
 	public String getStringFromFile(String filePath, String encodeType) {
 		Path path = Paths.get(filePath);
 		StringBuffer result = new StringBuffer();
@@ -51,425 +73,500 @@ public class Typing {
 		return getStringFromFile(filePath, StandardCharsets.UTF_8.displayName());
 	}
 
-	public void type(char character) {
-		switch (character) {
-		case 'a':
-			doType(KeyEvent.VK_A);
+	public void type(String str) {
+		switch (str) {
+		case "a":
+			r.keyPress(KeyEvent.VK_A);
+			r.keyRelease(KeyEvent.VK_A);
 			break;
-		case 'b':
-			doType(KeyEvent.VK_B);
+		case "b":
+			r.keyPress(KeyEvent.VK_B);
+			r.keyRelease(KeyEvent.VK_B);
 			break;
-		case 'c':
-			doType(KeyEvent.VK_C);
+		case "c":
+			r.keyPress(KeyEvent.VK_C);
+			r.keyRelease(KeyEvent.VK_C);
 			break;
-		case 'd':
-			doType(KeyEvent.VK_D);
+		case "d":
+			r.keyPress(KeyEvent.VK_D);
+			r.keyRelease(KeyEvent.VK_D);
 			break;
-		case 'e':
-			doType(KeyEvent.VK_E);
+		case "e":
+			r.keyPress(KeyEvent.VK_E);
+			r.keyRelease(KeyEvent.VK_E);
 			break;
-		case 'f':
-			doType(KeyEvent.VK_F);
+		case "f":
+			r.keyPress(KeyEvent.VK_F);
+			r.keyRelease(KeyEvent.VK_F);
 			break;
-		case 'g':
-			doType(KeyEvent.VK_G);
+		case "g":
+			r.keyPress(KeyEvent.VK_G);
+			r.keyRelease(KeyEvent.VK_G);
 			break;
-		case 'h':
-			doType(KeyEvent.VK_H);
+		case "h":
+			r.keyPress(KeyEvent.VK_H);
+			r.keyRelease(KeyEvent.VK_H);
 			break;
-		case 'i':
-			doType(KeyEvent.VK_I);
+		case "i":
+			r.keyPress(KeyEvent.VK_I);
+			r.keyRelease(KeyEvent.VK_I);
 			break;
-		case 'j':
-			doType(KeyEvent.VK_J);
+		case "j":
+			r.keyPress(KeyEvent.VK_J);
+			r.keyRelease(KeyEvent.VK_J);
 			break;
-		case 'k':
-			doType(KeyEvent.VK_K);
+		case "k":
+			r.keyPress(KeyEvent.VK_K);
+			r.keyRelease(KeyEvent.VK_K);
 			break;
-		case 'l':
-			doType(KeyEvent.VK_L);
+		case "l":
+			r.keyPress(KeyEvent.VK_L);
+			r.keyRelease(KeyEvent.VK_L);
 			break;
-		case 'm':
-			doType(KeyEvent.VK_M);
+		case "m":
+			r.keyPress(KeyEvent.VK_M);
+			r.keyRelease(KeyEvent.VK_M);
 			break;
-		case 'n':
-			doType(KeyEvent.VK_N);
+		case "n":
+			r.keyPress(KeyEvent.VK_N);
+			r.keyRelease(KeyEvent.VK_N);
 			break;
-		case 'o':
-			doType(KeyEvent.VK_O);
+		case "o":
+			r.keyPress(KeyEvent.VK_O);
+			r.keyRelease(KeyEvent.VK_O);
 			break;
-		case 'p':
-			doType(KeyEvent.VK_P);
+		case "p":
+			r.keyPress(KeyEvent.VK_P);
+			r.keyRelease(KeyEvent.VK_P);
 			break;
-		case 'q':
-			doType(KeyEvent.VK_Q);
+		case "q":
+			r.keyPress(KeyEvent.VK_Q);
+			r.keyRelease(KeyEvent.VK_Q);
 			break;
-		case 'r':
-			doType(KeyEvent.VK_R);
+		case "r":
+			r.keyPress(KeyEvent.VK_R);
+			r.keyRelease(KeyEvent.VK_R);
 			break;
-		case 's':
-			doType(KeyEvent.VK_S);
+		case "s":
+			r.keyPress(KeyEvent.VK_S);
+			r.keyRelease(KeyEvent.VK_S);
 			break;
-		case 't':
-			doType(KeyEvent.VK_T);
+		case "t":
+			r.keyPress(KeyEvent.VK_T);
+			r.keyRelease(KeyEvent.VK_T);
 			break;
-		case 'u':
-			doType(KeyEvent.VK_U);
+		case "u":
+			r.keyPress(KeyEvent.VK_U);
+			r.keyRelease(KeyEvent.VK_U);
 			break;
-		case 'v':
-			doType(KeyEvent.VK_V);
+		case "v":
+			r.keyPress(KeyEvent.VK_V);
+			r.keyRelease(KeyEvent.VK_V);
 			break;
-		case 'w':
-			doType(KeyEvent.VK_W);
+		case "w":
+			r.keyPress(KeyEvent.VK_W);
+			r.keyRelease(KeyEvent.VK_W);
 			break;
-		case 'x':
-			doType(KeyEvent.VK_X);
+		case "x":
+			r.keyPress(KeyEvent.VK_X);
+			r.keyRelease(KeyEvent.VK_X);
 			break;
-		case 'y':
-			doType(KeyEvent.VK_Y);
+		case "y":
+			r.keyPress(KeyEvent.VK_Y);
+			r.keyRelease(KeyEvent.VK_Y);
 			break;
-		case 'z':
-			doType(KeyEvent.VK_Z);
+		case "z":
+			r.keyPress(KeyEvent.VK_Z);
+			r.keyRelease(KeyEvent.VK_Z);
 			break;
-		case 'A':
+		case "A":
 			r.keyPress(KeyEvent.VK_SHIFT);
-			doType(KeyEvent.VK_A);
+			r.keyPress(KeyEvent.VK_A);
+			r.keyRelease(KeyEvent.VK_A);
 			r.keyRelease(KeyEvent.VK_SHIFT);
 			break;
-		case 'B':
+		case "B":
 			r.keyPress(KeyEvent.VK_SHIFT);
-			doType(KeyEvent.VK_B);
+			r.keyPress(KeyEvent.VK_B);
+			r.keyRelease(KeyEvent.VK_B);
 			r.keyRelease(KeyEvent.VK_SHIFT);
 			break;
-		case 'C':
+		case "C":
 			r.keyPress(KeyEvent.VK_SHIFT);
-			doType(KeyEvent.VK_C);
+			r.keyPress(KeyEvent.VK_C);
+			r.keyRelease(KeyEvent.VK_C);
 			r.keyRelease(KeyEvent.VK_SHIFT);
 			break;
-		case 'D':
+		case "D":
 			r.keyPress(KeyEvent.VK_SHIFT);
-			doType(KeyEvent.VK_D);
+			r.keyPress(KeyEvent.VK_D);
+			r.keyRelease(KeyEvent.VK_D);
 			r.keyRelease(KeyEvent.VK_SHIFT);
 			break;
-		case 'E':
+		case "E":
 			r.keyPress(KeyEvent.VK_SHIFT);
-			doType(KeyEvent.VK_E);
+			r.keyPress(KeyEvent.VK_E);
+			r.keyRelease(KeyEvent.VK_E);
 			r.keyRelease(KeyEvent.VK_SHIFT);
 			break;
-		case 'F':
+		case "F":
 			r.keyPress(KeyEvent.VK_SHIFT);
-			doType(KeyEvent.VK_F);
+			r.keyPress(KeyEvent.VK_F);
+			r.keyRelease(KeyEvent.VK_F);
 			r.keyRelease(KeyEvent.VK_SHIFT);
 			break;
-		case 'G':
+		case "G":
 			r.keyPress(KeyEvent.VK_SHIFT);
-			doType(KeyEvent.VK_G);
+			r.keyPress(KeyEvent.VK_G);
+			r.keyRelease(KeyEvent.VK_G);
 			r.keyRelease(KeyEvent.VK_SHIFT);
 			break;
-		case 'H':
+		case "H":
 			r.keyPress(KeyEvent.VK_SHIFT);
-			doType(KeyEvent.VK_H);
+			r.keyPress(KeyEvent.VK_H);
+			r.keyRelease(KeyEvent.VK_H);
 			r.keyRelease(KeyEvent.VK_SHIFT);
 			break;
-		case 'I':
+		case "I":
 			r.keyPress(KeyEvent.VK_SHIFT);
-			doType(KeyEvent.VK_I);
+			r.keyPress(KeyEvent.VK_I);
+			r.keyRelease(KeyEvent.VK_I);
 			r.keyRelease(KeyEvent.VK_SHIFT);
 			break;
-		case 'J':
+		case "J":
 			r.keyPress(KeyEvent.VK_SHIFT);
-			doType(KeyEvent.VK_J);
+			r.keyPress(KeyEvent.VK_J);
+			r.keyRelease(KeyEvent.VK_J);
 			r.keyRelease(KeyEvent.VK_SHIFT);
 			break;
-		case 'K':
+		case "K":
 			r.keyPress(KeyEvent.VK_SHIFT);
-			doType(KeyEvent.VK_K);
+			r.keyPress(KeyEvent.VK_K);
+			r.keyRelease(KeyEvent.VK_K);
 			r.keyRelease(KeyEvent.VK_SHIFT);
 			break;
-		case 'L':
+		case "L":
 			r.keyPress(KeyEvent.VK_SHIFT);
-			doType(KeyEvent.VK_L);
+			r.keyPress(KeyEvent.VK_L);
+			r.keyRelease(KeyEvent.VK_L);
 			r.keyRelease(KeyEvent.VK_SHIFT);
 			break;
-		case 'M':
+		case "M":
 			r.keyPress(KeyEvent.VK_SHIFT);
-			doType(KeyEvent.VK_M);
+			r.keyPress(KeyEvent.VK_M);
+			r.keyRelease(KeyEvent.VK_M);
 			r.keyRelease(KeyEvent.VK_SHIFT);
 			break;
-		case 'N':
+		case "N":
 			r.keyPress(KeyEvent.VK_SHIFT);
-			doType(KeyEvent.VK_N);
+			r.keyPress(KeyEvent.VK_N);
+			r.keyRelease(KeyEvent.VK_N);
 			r.keyRelease(KeyEvent.VK_SHIFT);
 			break;
-		case 'O':
+		case "O":
 			r.keyPress(KeyEvent.VK_SHIFT);
-			doType(KeyEvent.VK_O);
+			r.keyPress(KeyEvent.VK_O);
+			r.keyRelease(KeyEvent.VK_O);
 			r.keyRelease(KeyEvent.VK_SHIFT);
 			break;
-		case 'P':
+		case "P":
 			r.keyPress(KeyEvent.VK_SHIFT);
-			doType(KeyEvent.VK_P);
+			r.keyPress(KeyEvent.VK_P);
+			r.keyRelease(KeyEvent.VK_P);
 			r.keyRelease(KeyEvent.VK_SHIFT);
 			break;
-		case 'Q':
+		case "Q":
 			r.keyPress(KeyEvent.VK_SHIFT);
-			doType(KeyEvent.VK_Q);
+			r.keyPress(KeyEvent.VK_Q);
+			r.keyRelease(KeyEvent.VK_Q);
 			r.keyRelease(KeyEvent.VK_SHIFT);
 			break;
-		case 'R':
+		case "R":
 			r.keyPress(KeyEvent.VK_SHIFT);
-			doType(KeyEvent.VK_R);
+			r.keyPress(KeyEvent.VK_R);
+			r.keyRelease(KeyEvent.VK_R);
 			r.keyRelease(KeyEvent.VK_SHIFT);
 			break;
-		case 'S':
+		case "S":
 			r.keyPress(KeyEvent.VK_SHIFT);
-			doType(KeyEvent.VK_S);
+			r.keyPress(KeyEvent.VK_S);
+			r.keyRelease(KeyEvent.VK_S);
 			r.keyRelease(KeyEvent.VK_SHIFT);
 			break;
-		case 'T':
+		case "T":
 			r.keyPress(KeyEvent.VK_SHIFT);
-			doType(KeyEvent.VK_T);
+			r.keyPress(KeyEvent.VK_T);
+			r.keyRelease(KeyEvent.VK_T);
 			r.keyRelease(KeyEvent.VK_SHIFT);
 			break;
-		case 'U':
+		case "U":
 			r.keyPress(KeyEvent.VK_SHIFT);
-			doType(KeyEvent.VK_U);
+			r.keyPress(KeyEvent.VK_U);
+			r.keyRelease(KeyEvent.VK_U);
 			r.keyRelease(KeyEvent.VK_SHIFT);
 			break;
-		case 'V':
+		case "V":
 			r.keyPress(KeyEvent.VK_SHIFT);
-			doType(KeyEvent.VK_V);
+			r.keyPress(KeyEvent.VK_V);
+			r.keyRelease(KeyEvent.VK_V);
 			r.keyRelease(KeyEvent.VK_SHIFT);
 			break;
-		case 'W':
+		case "W":
 			r.keyPress(KeyEvent.VK_SHIFT);
-			doType(KeyEvent.VK_W);
+			r.keyPress(KeyEvent.VK_W);
+			r.keyRelease(KeyEvent.VK_W);
 			r.keyRelease(KeyEvent.VK_SHIFT);
 			break;
-		case 'X':
+		case "X":
 			r.keyPress(KeyEvent.VK_SHIFT);
-			doType(KeyEvent.VK_X);
+			r.keyPress(KeyEvent.VK_X);
+			r.keyRelease(KeyEvent.VK_X);
 			r.keyRelease(KeyEvent.VK_SHIFT);
 			break;
-		case 'Y':
+		case "Y":
 			r.keyPress(KeyEvent.VK_SHIFT);
-			doType(KeyEvent.VK_Y);
+			r.keyPress(KeyEvent.VK_Y);
+			r.keyRelease(KeyEvent.VK_Y);
 			r.keyRelease(KeyEvent.VK_SHIFT);
 			break;
-		case 'Z':
+		case "Z":
 			r.keyPress(KeyEvent.VK_SHIFT);
-			doType(KeyEvent.VK_Z);
+			r.keyPress(KeyEvent.VK_Z);
+			r.keyRelease(KeyEvent.VK_Z);
 			r.keyRelease(KeyEvent.VK_SHIFT);
 			break;
-		case '`':
-			doType(KeyEvent.VK_BACK_QUOTE);
+		case "`":
+			r.keyPress(KeyEvent.VK_BACK_QUOTE);
+			r.keyRelease(KeyEvent.VK_BACK_QUOTE);
 			break;
-		case '0':
-			doType(KeyEvent.VK_0);
+		case "0":
+			r.keyPress(KeyEvent.VK_0);
+			r.keyRelease(KeyEvent.VK_0);
 			break;
-		case '1':
-			doType(KeyEvent.VK_1);
+		case "1":
+			r.keyPress(KeyEvent.VK_1);
+			r.keyRelease(KeyEvent.VK_1);
 			break;
-		case '2':
-			doType(KeyEvent.VK_2);
+		case "2":
+			r.keyPress(KeyEvent.VK_2);
+			r.keyRelease(KeyEvent.VK_2);
 			break;
-		case '3':
-			doType(KeyEvent.VK_3);
+		case "3":
+			r.keyPress(KeyEvent.VK_3);
+			r.keyRelease(KeyEvent.VK_3);
 			break;
-		case '4':
-			doType(KeyEvent.VK_4);
+		case "4":
+			r.keyPress(KeyEvent.VK_4);
+			r.keyRelease(KeyEvent.VK_4);
 			break;
-		case '5':
-			doType(KeyEvent.VK_5);
+		case "5":
+			r.keyPress(KeyEvent.VK_5);
+			r.keyRelease(KeyEvent.VK_5);
 			break;
-		case '6':
-			doType(KeyEvent.VK_6);
+		case "6":
+			r.keyPress(KeyEvent.VK_6);
+			r.keyRelease(KeyEvent.VK_6);
 			break;
-		case '7':
-			doType(KeyEvent.VK_7);
+		case "7":
+			r.keyPress(KeyEvent.VK_7);
+			r.keyRelease(KeyEvent.VK_7);
 			break;
-		case '8':
-			doType(KeyEvent.VK_8);
+		case "8":
+			r.keyPress(KeyEvent.VK_8);
+			r.keyRelease(KeyEvent.VK_8);
 			break;
-		case '9':
-			doType(KeyEvent.VK_9);
+		case "9":
+			r.keyPress(KeyEvent.VK_9);
+			r.keyRelease(KeyEvent.VK_9);
 			break;
-		case '-':
-			doType(KeyEvent.VK_MINUS);
+		case "-":
+			r.keyPress(KeyEvent.VK_MINUS);
+			r.keyRelease(KeyEvent.VK_MINUS);
 			break;
-		case '=':
-			doType(KeyEvent.VK_EQUALS);
+		case "=":
+			r.keyPress(KeyEvent.VK_EQUALS);
+			r.keyRelease(KeyEvent.VK_EQUALS);
 			break;
-		case '~':
+		case "~":
 			r.keyPress(KeyEvent.VK_SHIFT);
-			doType(KeyEvent.VK_BACK_QUOTE);
+			r.keyPress(KeyEvent.VK_BACK_QUOTE);
+			r.keyRelease(KeyEvent.VK_BACK_QUOTE);
 			r.keyRelease(KeyEvent.VK_SHIFT);
 			break;
-		case '!':
+		case "!":
 			r.keyPress(KeyEvent.VK_SHIFT);
-			doType(KeyEvent.VK_1);
+			r.keyPress(KeyEvent.VK_1);
+			r.keyRelease(KeyEvent.VK_1);
 			r.keyRelease(KeyEvent.VK_SHIFT);
 			break;
-		case '@':
+		case "@":
 			r.keyPress(KeyEvent.VK_SHIFT);
-			doType(KeyEvent.VK_2);
+			r.keyPress(KeyEvent.VK_2);
+			r.keyRelease(KeyEvent.VK_2);
 			r.keyRelease(KeyEvent.VK_SHIFT);
 			break;
-		case '#':
+		case "#":
 			r.keyPress(KeyEvent.VK_SHIFT);
-			doType(KeyEvent.VK_3);
+			r.keyPress(KeyEvent.VK_3);
+			r.keyRelease(KeyEvent.VK_3);
 			r.keyRelease(KeyEvent.VK_SHIFT);
 			break;
-		case '$':
+		case "$":
 			r.keyPress(KeyEvent.VK_SHIFT);
-			doType(KeyEvent.VK_4);
+			r.keyPress(KeyEvent.VK_4);
+			r.keyRelease(KeyEvent.VK_4);
 			r.keyRelease(KeyEvent.VK_SHIFT);
 			break;
-		case '%':
+		case "%":
 			r.keyPress(KeyEvent.VK_SHIFT);
-			doType(KeyEvent.VK_5);
+			r.keyPress(KeyEvent.VK_5);
+			r.keyRelease(KeyEvent.VK_5);
 			r.keyRelease(KeyEvent.VK_SHIFT);
 			break;
-		case '^':
+		case "^":
 			r.keyPress(KeyEvent.VK_SHIFT);
-			doType(KeyEvent.VK_6);
+			r.keyPress(KeyEvent.VK_6);
+			r.keyRelease(KeyEvent.VK_6);
 			r.keyRelease(KeyEvent.VK_SHIFT);
 			break;
-		case '&':
+		case "&":
 			r.keyPress(KeyEvent.VK_SHIFT);
-			doType(KeyEvent.VK_7);
+			r.keyPress(KeyEvent.VK_7);
+			r.keyRelease(KeyEvent.VK_7);
 			r.keyRelease(KeyEvent.VK_SHIFT);
 			break;
-		case '*':
+		case "*":
 			r.keyPress(KeyEvent.VK_SHIFT);
-			doType(KeyEvent.VK_8);
+			r.keyPress(KeyEvent.VK_8);
+			r.keyRelease(KeyEvent.VK_8);
 			r.keyRelease(KeyEvent.VK_SHIFT);
 			break;
-		case '(':
+		case "(":
 			r.keyPress(KeyEvent.VK_SHIFT);
-			doType(KeyEvent.VK_9);
+			r.keyPress(KeyEvent.VK_9);
+			r.keyRelease(KeyEvent.VK_9);
 			r.keyRelease(KeyEvent.VK_SHIFT);
 			break;
-		case ')':
+		case ")":
 			r.keyPress(KeyEvent.VK_SHIFT);
-			doType(KeyEvent.VK_0);
+			r.keyPress(KeyEvent.VK_0);
+			r.keyRelease(KeyEvent.VK_0);
 			r.keyRelease(KeyEvent.VK_SHIFT);
 			break;
-		case '_':
+		case "_":
 			r.keyPress(KeyEvent.VK_SHIFT);
-			doType(KeyEvent.VK_MINUS);
+			r.keyPress(KeyEvent.VK_MINUS);
+			r.keyRelease(KeyEvent.VK_MINUS);
 			r.keyRelease(KeyEvent.VK_SHIFT);
 			break;
-		case '+':
+		case "+":
 			r.keyPress(KeyEvent.VK_SHIFT);
-			doType(KeyEvent.VK_EQUALS);
+			r.keyPress(KeyEvent.VK_EQUALS);
+			r.keyRelease(KeyEvent.VK_EQUALS);
 			r.keyRelease(KeyEvent.VK_SHIFT);
-		case '\t':
-			doType(KeyEvent.VK_TAB);
+		case "\t":
+			r.keyPress(KeyEvent.VK_TAB);
+			r.keyRelease(KeyEvent.VK_TAB);
 			break;
-		case '\n':
+		case "\n":
 			r.keyPress(KeyEvent.VK_ENTER);
 			r.keyRelease(KeyEvent.VK_ENTER);
 			break;
-		case '\r':
+		case "\r":
 			r.keyPress(KeyEvent.VK_ENTER);
 			r.keyRelease(KeyEvent.VK_ENTER);
 			break;
-		case '[':
-			doType(KeyEvent.VK_OPEN_BRACKET);
+		case "\r\n":
+			r.keyPress(KeyEvent.VK_ENTER);
+			r.keyRelease(KeyEvent.VK_ENTER);
 			break;
-		case ']':
-			doType(KeyEvent.VK_CLOSE_BRACKET);
+		case "[":
+			r.keyPress(KeyEvent.VK_OPEN_BRACKET);
+			r.keyRelease(KeyEvent.VK_OPEN_BRACKET);
 			break;
-		case '\\':
-			doType(KeyEvent.VK_BACK_SLASH);
+		case "]":
+			r.keyPress(KeyEvent.VK_CLOSE_BRACKET);
+			r.keyRelease(KeyEvent.VK_CLOSE_BRACKET);
 			break;
-		case '{':
+		case "\\":
+			r.keyPress(KeyEvent.VK_BACK_SLASH);
+			r.keyRelease(KeyEvent.VK_BACK_SLASH);
+			break;
+		case "{":
 			r.keyPress(KeyEvent.VK_SHIFT);
-			doType(KeyEvent.VK_OPEN_BRACKET);
+			r.keyPress(KeyEvent.VK_OPEN_BRACKET);
+			r.keyRelease(KeyEvent.VK_OPEN_BRACKET);
 			r.keyRelease(KeyEvent.VK_SHIFT);
 			break;
-		case '}':
+		case "}":
 			r.keyPress(KeyEvent.VK_SHIFT);
-			doType(KeyEvent.VK_CLOSE_BRACKET);
+			r.keyPress(KeyEvent.VK_CLOSE_BRACKET);
+			r.keyRelease(KeyEvent.VK_CLOSE_BRACKET);
 			r.keyRelease(KeyEvent.VK_SHIFT);
 			break;
-		case '|':
+		case "|":
 			r.keyPress(KeyEvent.VK_SHIFT);
-			doType(KeyEvent.VK_BACK_SLASH);
+			r.keyPress(KeyEvent.VK_BACK_SLASH);
+			r.keyRelease(KeyEvent.VK_BACK_SLASH);
 			r.keyRelease(KeyEvent.VK_SHIFT);
 			break;
-		case ';':
-			doType(KeyEvent.VK_SEMICOLON);
+		case ";":
+			r.keyPress(KeyEvent.VK_SEMICOLON);
+			r.keyRelease(KeyEvent.VK_SEMICOLON);
 			break;
-		case ':':
+		case ":":
 			r.keyPress(KeyEvent.VK_SHIFT);
-			doType(KeyEvent.VK_SEMICOLON);
+			r.keyPress(KeyEvent.VK_SEMICOLON);
+			r.keyRelease(KeyEvent.VK_SEMICOLON);
 			r.keyRelease(KeyEvent.VK_SHIFT);
 			break;
-		case '\'':
-			doType(KeyEvent.VK_QUOTE);
+		case "\'":
+			r.keyPress(KeyEvent.VK_QUOTE);
+			r.keyRelease(KeyEvent.VK_QUOTE);
 			break;
-		case '\"':
+		case "\"":
 			r.keyPress(KeyEvent.VK_SHIFT);
-			doType(KeyEvent.VK_QUOTE);
+			r.keyPress(KeyEvent.VK_QUOTE);
+			r.keyRelease(KeyEvent.VK_QUOTE);
 			r.keyRelease(KeyEvent.VK_SHIFT);
 			break;
-		case ',':
-			doType(KeyEvent.VK_COMMA);
+		case ",":
+			r.keyPress(KeyEvent.VK_COMMA);
+			r.keyRelease(KeyEvent.VK_COMMA);
 			break;
-		case '<':
+		case "<":
 			r.keyPress(KeyEvent.VK_SHIFT);
-			doType(KeyEvent.VK_COMMA);
+			r.keyPress(KeyEvent.VK_COMMA);
+			r.keyRelease(KeyEvent.VK_COMMA);
 			r.keyRelease(KeyEvent.VK_SHIFT);
 			break;
-		case '.':
-			doType(KeyEvent.VK_PERIOD);
+		case ".":
+			r.keyPress(KeyEvent.VK_PERIOD);
+			r.keyRelease(KeyEvent.VK_PERIOD);
 			break;
-		case '>':
+		case ">":
 			r.keyPress(KeyEvent.VK_SHIFT);
-			doType(KeyEvent.VK_PERIOD);
+			r.keyPress(KeyEvent.VK_PERIOD);
+			r.keyRelease(KeyEvent.VK_PERIOD);
 			r.keyRelease(KeyEvent.VK_SHIFT);
 			break;
-		case '/':
-			doType(KeyEvent.VK_SLASH);
+		case "/":
+			r.keyPress(KeyEvent.VK_SLASH);
+			r.keyRelease(KeyEvent.VK_SLASH);
 			break;
-		case '?':
+		case "?":
 			r.keyPress(KeyEvent.VK_SHIFT);
-			doType(KeyEvent.VK_SLASH);
+			r.keyPress(KeyEvent.VK_SLASH);
+			r.keyRelease(KeyEvent.VK_SLASH);
 			r.keyRelease(KeyEvent.VK_SHIFT);
 			break;
-		case ' ':
-			doType(KeyEvent.VK_SPACE);
+		case " ":
+			r.keyPress(KeyEvent.VK_SPACE);
+			r.keyRelease(KeyEvent.VK_SPACE);
 			break;
 		default:
-			System.out.println("Cannot type character " + character);
+			System.out.println("Cannot type character " + str);
 		}
 	}
 
-	public void type(CharSequence characters) {
-		int length = characters.length();
-		for (int i = 0; i < length; i++) {
-			char character = characters.charAt(i);
-			type(character);
-			try {
-				Thread.sleep(30L);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-
-	private void doType(int... keyCodes) {
-		doType(keyCodes, 0, keyCodes.length);
-	}
-
-	private void doType(int[] keyCodes, int offset, int length) {
-		if (length == 0)
-			return;
-
-		r.keyPress(keyCodes[offset]);
-		doType(keyCodes, offset + 1, length - 1);
-		r.keyRelease(keyCodes[offset]);
-	}
 }
