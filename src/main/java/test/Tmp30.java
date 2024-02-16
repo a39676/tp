07377df7771fd8026b8c0c56dccd52b1a597ee06
DavenChan <19781485+a39676@users.noreplay.github.com>
2024-02-16 10:47:11 +0800

    .

--- src/main/java/test/Tmp30.java
+++ src/main/java/test/Tmp30.java
@@ -1,7 +1,33 @@
 package test;
 
-public class Tmp30 extends Tmp29{
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Tmp30 extends Tmp29 {
 
 	public static void main(String[] args) {
+		StringBuffer sb = new StringBuffer("sometimes");
+		if (sb.length() <= 3) {
+			for (int i = 0; i < sb.length(); i++) {
+				sb.setCharAt(i, '_');
+			}
+		}
+		int length = sb.length();
+		int notReplaceCounting = length / 3;
+		if (notReplaceCounting < 3) {
+			notReplaceCounting = 3;
+		}
+		int replaceCount = length - notReplaceCounting;
+		int randomIndex = 0;
+		for (int i = 0; i < replaceCount; i++) {
+			randomIndex = ThreadLocalRandom.current().nextInt(0, sb.length());
+			char tmpChar = sb.charAt(randomIndex);
+			if (Character.isLetter(tmpChar)) {
+				sb.replace(randomIndex, randomIndex + 1, "_");
+			} else {
+				i--;
+				continue;
+			}
+		}
+		System.out.println(sb.toString());
 	}
 }
