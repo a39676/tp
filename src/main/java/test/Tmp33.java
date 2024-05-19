4c5060e1589d3f54b0c9a25bff403afeb00086b9
DavenChan <19781485+a39676@users.noreplay.github.com>
2024-05-19 11:01:53 +0800

    .

---
+++ src/main/java/test/Tmp33.java
@@ -0,0 +1,14 @@
+package test;
+
+import java.util.Date;
+
+public class Tmp33 {
+	
+	public static void main(String[] args) {
+		Date d = new Date(1705420800000L);
+		System.out.println(d);
+		d = new Date(1716013626897L);
+		System.out.println(d);
+	}
+
+}
