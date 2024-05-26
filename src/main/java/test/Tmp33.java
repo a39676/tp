9dd3e69270f83ff91b05d24f6411530827c651d7
Daven <19781485+a39676@users.noreply.github.com>
2024-05-27 00:11:58 +0800

    .

--- src/main/java/test/Tmp33.java
+++ src/main/java/test/Tmp33.java
@@ -3,12 +3,9 @@ package test;
 import java.util.Date;
 
 public class Tmp33 {
-	
+
 	public static void main(String[] args) {
-		Date d = new Date(1705420800000L);
-		System.out.println(d);
-		d = new Date(1545404023000L);
-		System.out.println(d);
+		System.out.println(new Date(1564034571105L));
 	}
 
 }
