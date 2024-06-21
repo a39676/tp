289459fd28ae1a7e13dd6a87c8f095988ea2c0d8
Daven <19781485+a39676@users.noreply.github.com>
2024-06-21 14:47:40 +0800

    .

--- src/main/java/test/Tmp33.java
+++ src/main/java/test/Tmp33.java
@@ -1,14 +1,11 @@
 package test;
 
-import java.io.File;
-
 public class Tmp33 {
 
 	public static void main(String[] args) {
-		String path = System.getProperty("user.home") ;
-		File f = new File(path);
-		System.out.println(f.exists());
-		System.out.println(f.getAbsolutePath());
+		String str = "$109.60B";
+		System.out.println(str.endsWith("B"));
+		System.out.println(str.startsWith("$9."));
 	}
 
 }
