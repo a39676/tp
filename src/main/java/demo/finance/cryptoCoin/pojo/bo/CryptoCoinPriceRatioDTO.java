be9767a68f657c9d3192d651ae8c0ee810d19fac
DavenChan <19781485+a39676@users.noreplay.github.com>
2024-08-27 00:16:32 +0800

    .

---
+++ src/main/java/demo/finance/cryptoCoin/pojo/bo/CryptoCoinPriceRatioDTO.java
@@ -0,0 +1,35 @@
+package demo.finance.cryptoCoin.pojo.bo;
+
+public class CryptoCoinPriceRatioDTO implements Comparable<CryptoCoinPriceRatioDTO> {
+
+	private String symbol;
+	private Double ratio;
+
+	public String getSymbol() {
+		return symbol;
+	}
+
+	public void setSymbol(String symbol) {
+		this.symbol = symbol;
+	}
+
+	public Double getRatio() {
+		return ratio;
+	}
+
+	public void setRatio(Double ratio) {
+		this.ratio = ratio;
+	}
+
+	@Override
+	public int compareTo(CryptoCoinPriceRatioDTO o) {
+		if (o.getRatio() > this.ratio) {
+			return 1;
+		} else if (o.getRatio() == this.ratio) {
+			return 0;
+		} else {
+			return -1;
+		}
+	}
+
+}
--- src/main/java/demo/finance/cryptoCoin/service/BinanceFutureUmKLine.java
+++ src/main/java/demo/finance/cryptoCoin/service/BinanceFutureUmKLine.java
@@ -1,13 +1,17 @@
 package demo.finance.cryptoCoin.service;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import demo.finance.cryptoCoin.pojo.bo.CryptoCoinPriceRatioDTO;
 import finance.common.pojo.type.IntervalType;
 import finance.cryptoCoin.pojo.bo.CryptoCoinPriceCommonDataBO;
 import net.sf.json.JSONArray;
@@ -15,9 +19,19 @@ import toolPack.ioHandle.FileUtilCustom;
 
 public class BinanceFutureUmKLine extends BinanceDataCommonService {
 
+	public static void main(String[] args) {
+//		setProxy();
+//		for (String symbol : symbols) {
+//			getKLineFromApiAndSave(symbol);
+//		}
+
+		checkAllGaps(4);
+	}
+
 	public static void getKLineFromApiAndSave(String symbol) {
+		System.out.println("Get K line from API, symbol: " + symbol);
 		BinanceFutureUmDataApiUnit apiUnit = new BinanceFutureUmDataApiUnit();
-		List<CryptoCoinPriceCommonDataBO> kLineData = apiUnit.getKLineDataFromApi(symbol, IntervalType.DAY_1, 100);
+		List<CryptoCoinPriceCommonDataBO> kLineData = apiUnit.getKLineDataFromApi(symbol, IntervalType.DAY_1, 499);
 		if (kLineData == null || kLineData.isEmpty()) {
 			System.err.println("Get K line failed. symbol: " + symbol);
 		}
@@ -31,6 +45,7 @@ public class BinanceFutureUmKLine extends BinanceDataCommonService {
 		FileUtilCustom fuc = new FileUtilCustom();
 		String filePath = getFileSavePath() + "/kLine/" + symbol + ".json";
 		fuc.byteToFile(str.getBytes(StandardCharsets.UTF_8), filePath);
+		System.out.println("Get K line from API DONE, symbol: " + symbol);
 	}
 
 	public static List<CryptoCoinPriceCommonDataBO> getKLineFromLocal(String symbol) {
@@ -47,6 +62,40 @@ public class BinanceFutureUmKLine extends BinanceDataCommonService {
 		}
 
 		return kLineData;
+	}
+
+	public static CryptoCoinPriceRatioDTO checkGap(String symbol, Integer dayGap) {
+		List<CryptoCoinPriceCommonDataBO> dataList = getKLineFromLocal(symbol);
+		if (dataList == null || dataList.isEmpty()) {
+			System.err.println("Symbol: " + symbol + ", data list empty");
+			return null;
+		}
+		if (dataList.size() < dayGap) {
+			System.err.println("Symbol: " + symbol + ", data list too short for: " + dayGap);
+			return null;
+		}
+		CryptoCoinPriceCommonDataBO startData = dataList.get(dataList.size() - 1 - dayGap);
+		CryptoCoinPriceCommonDataBO lastData = dataList.get(dataList.size() - 1);
+		CryptoCoinPriceRatioDTO dto = new CryptoCoinPriceRatioDTO();
+		BigDecimal gapRatio = lastData.getEndPrice().divide(startData.getStartPrice(), SCALE_FOR_RATE_CALCULATE,
+				RoundingMode.HALF_UP);
+		dto.setSymbol(symbol);
+		dto.setRatio(gapRatio.doubleValue());
+		System.out.println("Symbol: " + symbol + ", ratio: " + gapRatio);
+		return dto;
+	}
+
+	public static void checkAllGaps(Integer dayGap) {
+		List<CryptoCoinPriceRatioDTO> list = new ArrayList<>();
+		CryptoCoinPriceRatioDTO dto = null;
+		for (String symbol : symbols) {
+			dto = checkGap(symbol, dayGap);
+			if (dto != null) {
+				list.add(dto);
+			}
+		}
+		Collections.sort(list);
+		System.out.println(list);
 	}
 
 }
